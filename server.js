ce2780dc751f3b001e6fd543ebfb6b925134ba03
cahudhari <168970127+cahudhari@users.noreply.github.com>
2025-04-09 22:27:37 +0530

    server.js

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const path = require("path");
+const jobRoutes = require(path.resolve(__dirname, "./routes/JobRoutes"));
 
+dotenv.config();
 const app = express();
-app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-app.use("/api/jobs", require("./routes/JobRoutes"));
+app.use("/api/jobs", jobRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
